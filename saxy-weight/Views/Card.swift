2ebef8d22266c4736bdb807ce95978c266281ac4
Alan Sax <alan@lelander.com>
2018-04-05 22:01:19 -0500

    added tab navigator icons/screens. added VC files

---
+++ saxy-weight/Views/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  saxy-weight
+//
+//  Created by Alan Sax on 4/5/18.
+//  Copyright © 2018 Alan Sax. All rights reserved.
+//
+
+import UIKit
+
+class Card: UIView {
+
+    @IBOutlet weak var view: UIView!
+
+}
