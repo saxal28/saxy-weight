0505e61a81f551f8a781d482f231718483ff44ca
Alan Sax <alan@lelander.com>
2018-04-14 17:03:17 -0500

    setup logbook table / rows

--- saxy-weight/Controllers/TabNavigator/LogbookVC.swift
+++ saxy-weight/Controllers/TabNavigator/LogbookVC.swift
@@ -8,28 +8,37 @@
 
 import UIKit
 
-class LogbookVC: UIViewController {
+class LogbookVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        print(WeightService.instance.weights)
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-    */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return WeightService.instance.weights.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "weightCell", for: indexPath) as! WeightCell
+        let data = WeightService.instance.weights[indexPath.row]
+        
+        print("row!", indexPath.row)
+        
+        cell.updateCell(date: "12/02/1942", weight: "\(data.weight)", weightChange: "0.3")
+        
+        return cell
+    }
+
 
 }
